7d9f0a29c16774eb4f45044e8dc7e31e0f6b29f3
Tyler Torola <tyler@tjt.codes>
2017-02-11 21:48:33 -0800

    Start refactoring for Draft

--- build.jsx
+++ build.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Editor from './editor.jsx';
-import Toolbar from './toolbar.jsx';
+// import Toolbar from './toolbar.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const tokens = [
-    'foo',
-    'bar',
-    'baz',
-  ];
-  const editors = [];
+  // const tokens = [
+  //   'foo',
+  //   'bar',
+  //   'baz',
+  // ];
+  // const editors = [];
 
-  ReactDOM.render(<Editor ref={e => editors.push(e)} />, document.getElementById('editor-one'));
-  ReactDOM.render(<Editor ref={e => editors.push(e)} />, document.getElementById('editor-two'));
-  ReactDOM.render(<Toolbar tokens={tokens} editors={editors}/>, document.getElementById('toolbar'));
+  ReactDOM.render(<Editor />, document.getElementById('editor-one'));
+  ReactDOM.render(<Editor />, document.getElementById('editor-two'));
+  // ReactDOM.render(<Toolbar tokens={tokens} editors={editors}/>, document.getElementById('toolbar'));
 });
--- editor.jsx
+++ editor.jsx
@@ -1,53 +1,25 @@
 import React from 'react';
-import Quill from 'quill';
+import {Editor, EditorState} from 'draft-js';
 
-const QUILL_OPTIONS = {
-  formats: ['bold'],
-}
-
-class ScriptEditor extends React.Component {
+class TokenEditor extends React.Component {
   constructor(props) {
     super(props);
 
-    this.addToken = this.addToken.bind(this);
-    this.selectionChange = this.selectionChange.bind(this);
-    this.id = Math.random().toString();
+    this.state = {
+      script: EditorState.createEmpty()
+    };
+
+    this.onChange = this.onChange.bind(this);
   }
 
-  componentDidMount() {
-    this.quill = new Quill(this.input, QUILL_OPTIONS);
-    this.quill.on('text-change', this.textChange);
-    this.quill.on('selection-change', this.selectionChange);
-  }
-
-  // http://quilljs.com/docs/api/#text-change
-  textChange(delta, oldDelta, source) {
-    // debugger;
-  }
-
-  // http://quilljs.com/docs/api/#selection-change
-  selectionChange(range, oldRange, source) {
-    // debugger;
-  }
-
-  addToken(token) {
-    const { quill } = this;
-    const { index, length } = this.quill.getSelection(true);
-
-    quill.deleteText(index, length);
-    quill.insertText(index, token, { bold: true });
-    quill.setSelection(index + token.length);
+  onChange(newState) {
+    debugger;
+    this.setState({ script: newState })
   }
 
   render() {
-    return (
-      <div
-        data-id={this.id}
-        ref={(input) => this.input = input}
-        className="editor"
-      />
-    );
+    return <Editor editorState={this.state.script} onChange={this.onChange} />;
   }
-};
+}
 
-export default ScriptEditor;
+export default TokenEditor;
