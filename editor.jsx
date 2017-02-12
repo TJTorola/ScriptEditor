7a716fae43efa5e913894df65cf14c2f1bdd9fdb
Tyler Torola <tyler@tjt.codes>
2017-02-11 23:19:04 -0800

    Basic Draft editor

--- editor.jsx
+++ editor.jsx
@@ -1,24 +1,52 @@
 import React from 'react';
-import {Editor, EditorState} from 'draft-js';
+import {
+  Editor,
+  EditorState,
+  Modifier,
+} from 'draft-js';
 
 class TokenEditor extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      script: EditorState.createEmpty()
+      editorState: EditorState.createEmpty()
     };
 
-    this.onChange = this.onChange.bind(this);
+    this.setEditorState = (editorState) => this.setState({ editorState });
+    this.addToken = this.addToken.bind(this);
   }
 
-  onChange(newState) {
-    debugger;
-    this.setState({ script: newState })
+  addToken() {
+    const { editorState } = this.state,
+          contentState = editorState.getCurrentContent(),
+          selectionState = editorState.getSelection();
+
+    const newContentState = Modifier.replaceText(
+      contentState,
+      selectionState,
+      'token'
+    );
+
+    const newEditorState = EditorState.createWithContent(newContentState);
+    this.setEditorState(newEditorState);
   }
 
   render() {
-    return <Editor editorState={this.state.script} onChange={this.onChange} />;
+    return (
+      <div>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.setEditorState}
+        />
+        <div
+          onMouseDown={ (e) => e.preventDefault() }
+          onClick={ this.addToken }
+        >
+          Add Token
+        </div>
+      </div>
+    );
   }
 }
 
