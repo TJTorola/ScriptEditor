8fc24c3117f0a42ac336f2347549265eecf7c6d9
Tyler Torola <tyler@tjt.codes>
2017-02-16 11:01:43 -0800

    Start abstracting toolbar

--- src/toolbar.jsx
+++ src/toolbar.jsx
@@ -1,37 +1,48 @@
 import React from 'react';
 
+const ADD_TOKEN = 'ADD_TOKEN';
+const ADD_TEXT = 'ADD_TEXT';
+const CUSTOM = 'CUSTOM';
+
 const findEditorId = (domNode) => {
   if (!domNode) return;
   if (domNode.dataset.id) return domNode.dataset.id;
 
   return findEditorId(domNode.parentElement);
-}
-
-const addToken = (editors, token) => () => {
-  const id = findEditorId(document.activeElement);
-  if (!id) return;
-
-  editors.forEach(editor => {
-    if (editor.id === id) {
-      editor.addToken(token)();
-    }
-  });
 };
 
-const tokenButton = editors => (token, idx) => (
+const addToken = (editor) => (text, token) => editor.addToken(text, token);
+const addText = (editor) => (text) => editor.addText(text)
+
+const handleClick = (editors, action) => (e) => {
+  const id = findEditorId(document.activeElement);
+  const editor = editors.find(editor => editor.id === id);
+  if (!editor) return;
+
+  switch (action.type) {
+    case ADD_TOKEN:
+      return addToken(editor)(action.text, action.token);
+    case ADD_TEXT:
+      return addText(editor)(action.text)
+    case CUSTOM:
+      return action.callback(e, addToken(editor), addText(editor))
+  };
+};
+
+const actionButton = editors => (action, idx) => (
   <span
-    className="token-button"
+    className="action-button"
     key={idx}
     onMouseDown={e => e.preventDefault()}
-    onClick={addToken(editors, token)}
+    onClick={handleClick(editors, action)}
   >
-    +{ token }
+    +{ action.text }
   </span>
 );
 
-const Toolbar = ({ tokens, editors }) => (
+const Toolbar = ({ actions, editors }) => (
   <div className="toolbar">
-    { tokens.map(tokenButton(editors)) }
+    { actions.map(actionButton(editors)) }
   </div>
 );
 
